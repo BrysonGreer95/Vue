7de054685999a113349d0a291703c4d362d9fa25
SapperCayne <bryson.greer2014@gmail.com>
2019-11-26 17:07:26 -0500

    Editing main for proper rendering

--- video-browser/src/main.js
+++ video-browser/src/main.js
@@ -3,6 +3,11 @@ import App from './App.vue'
 
 Vue.config.productionTip = false
 
+// new Vue({
+//   render: function(createElement){
+//     return createElement(App);
+//   }
+// This block of code above does the same thing as what is below
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  render: h => h(App)
+}).$mount('#app');
