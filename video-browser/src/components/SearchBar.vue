df9841563ff8642f79474e4d89cd5443fb68ea1f
SapperCayne <bryson.greer2014@gmail.com>
2019-12-04 20:47:41 -0500

    Created methods for calling YouTube API

--- video-browser/src/components/SearchBar.vue
+++ video-browser/src/components/SearchBar.vue
@@ -1,16 +1,21 @@
 <template>
     <div>
-        <input v-on:input="whenUserTypes"/>
+        <!-- This @input means, when there is input (typing), execute the method "whenUserTypes" -->
+        <input @input="whenUserTypes"/>
     </div>
 </template>
 
+// This is the script of the code below. Basically where we add functionality, and make connections.
 <script>
 export default{
     name: 'SearchBar',
     methods: {
+        // This is the method, "whenUserTypes" which we referenced above on the @input. 
     whenUserTypes: function(event) {
-        console.log(event.target.value);
-    }
+        // this next line means, when "this" (the function), emits a change, update the value termChange
+        // basically reads as, this function should emit a change called termChange, when event.target.value changes.
+        this.$emit('termChange', event.target.value);
+}
     }
 };
 </script>
